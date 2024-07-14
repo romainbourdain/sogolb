428fad37f482081fde3f3694cca73810666b83e5
LOISGALLAUD <loisgallaud@hotmail.com>
2024-07-14 22:12:24 +0200

    add: search bar with icon

--- app/(main)/explore/page.tsx
+++ app/(main)/explore/page.tsx
@@ -1,5 +1,4 @@
 import { PageLayout } from "@/components/tailwind/page-layout";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import type { PageParams } from "@/types/next";
 import { Search } from "lucide-react";
@@ -8,16 +7,19 @@ export default async function RoutePage(props: PageParams<{}>) {
   return (
     <PageLayout className="h-full">
       {/* Search bar */}
-      <div className="flex justify-center items-center gap-2 bg-yellow-600">
-        <Input
-          className="bg-warning-foreground"
-          type="search"
-          placeholder="Rechercher un article, une formation, un parcours..."
-        />
-        <Button size="sm" className="mt-2 bg-slate-400">
-          <Search size={16} />
-        </Button>
+      <div className="flex justify-center items-center py-5">
+        <div className="relative w-full">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <Search className="w-5 h-5 text-muted-foreground" />
+          </div>
+          <Input
+            type="search"
+            placeholder="Rechercher un article, une formation, un parcours..."
+            className="pl-10"
+          />
+        </div>
       </div>
+
       {/* Articles */}
       {/* Formations */}
       {/* Curriculum */}
