d97363ec1718aa1f1de416583cfa5cbe3c93494b
LOISGALLAUD <loisgallaud@hotmail.com>
2024-07-21 17:13:57 +0200

    add: new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,29 @@ import { config } from "@/config";
 import { cn } from "@/lib/utils";
 import type { LayoutParams } from "@/types/next";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { Provider } from "./provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const marsel = localFont({
+  src: [
+    {
+      path: "../public/fonts/marsel/MarselTRIAL-Regular-BF6618a1b45dda4.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/marsel/MarselTRIAL-Bold-BF6618a1b4158b8.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/marsel/MarselTRIAL-Thin-BF6618a1b472b09.otf",
+      weight: "100",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: config.title,
@@ -19,7 +37,7 @@ export default async function RootLayout({ children }: LayoutParams<{}>) {
       <head>
         <link rel="icon" {...config} />
       </head>
-      <body className={cn("min-h-screen h-full", inter.className)}>
+      <body className={cn("min-h-screen h-full", marsel.className)}>
         <Provider>{children}</Provider>
       </body>
     </html>
---
+++ src/features/resource/resource-card.tsx
@@ -0,0 +1,52 @@
+import { Typography } from "@/components/ui/typography";
+import { BookOpen, GraduationCap, LucideIcon, Newspaper } from "lucide-react";
+import Image from "next/image";
+
+interface ResourceCardProps {
+  title: string;
+  content: string;
+}
+
+const variants: {
+  [key: string]: { color: string; icon: LucideIcon };
+} = {
+  article: { color: "pink", icon: Newspaper },
+  formation: { color: "purple", icon: GraduationCap },
+  lesson: { color: "orange", icon: BookOpen },
+};
+
+export const ResourceCard = ({
+  resource,
+  variant,
+}: {
+  resource: ResourceCardProps;
+  variant: keyof typeof variants;
+}) => {
+  const Icon: LucideIcon = variants[variant].icon;
+  return (
+    <div
+      className="relative flex flex-col justify-between
+       border-secondary border-4 rounded-xl p-3
+       aspect-square overflow-hidden size-36 md:size-52"
+    >
+      {/* Background fallback */}
+      <Image
+        src={`/images/${variants[variant].color}-cover.svg`}
+        alt="cover"
+        layout="fill"
+        objectFit="cover"
+        className="absolute inset-0 z-[-1]"
+      />
+
+      {/* Top right icon */}
+      <div className="flex justify-end items-center w-full">
+        <Icon className="text-white" />
+      </div>
+
+      {/* Title */}
+      <Typography variant="h2" className="font-semibold text-white text-md">
+        {resource.title}
+      </Typography>
+    </div>
+  );
+};
