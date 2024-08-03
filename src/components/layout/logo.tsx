7878c8329fdd620b3b059311f3f292beb60dc93d
Romain Bourdain <romain.bourdain93@gmail.com>
2024-08-03 23:20:07 +0200

    updated colors

--- src/components/layout/logo.tsx
+++ src/components/layout/logo.tsx
@@ -3,7 +3,7 @@ import { FaKiwiBird } from "react-icons/fa";
 
 export const Logo = () => {
   return (
-    <Link href="/" className="flex items-center gap-2">
+    <Link href="/home" className="flex items-center gap-2">
       <FaKiwiBird size={30} className="text-primary" />
       <span className="text-lg font-semibold">Kiwi</span>
     </Link>
